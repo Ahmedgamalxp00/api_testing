03cf2a86db62a608078cb16d21a35b1146b0837d
AhmedGamalxp <aahmed.ggamal00@gmail.com>
2023-12-30 19:09:09 +0200

    fix some errors

--- lib/features/presentation/views/widgets/custom_textformfield.dart
+++ lib/features/presentation/views/widgets/custom_textformfield.dart
@@ -20,31 +20,41 @@ class CustomFormField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(bottom: 20),
       child: TextFormField(
-          onFieldSubmitted: onFieldSubmitted,
-          onSaved: onSaved,
-          maxLines: null,
-          initialValue: initialValue,
-          keyboardType: textInputType,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          validator: (value) {
-            if (value!.isEmpty) {
-              return '$hint can\'t be null';
-            }
-            return null;
-          },
-          onChanged: onchanged,
-          onTapOutside: (event) {
-            FocusManager.instance.primaryFocus?.unfocus();
-          },
-          decoration: InputDecoration(
-            label: Text(hint),
-            labelStyle: const TextStyle(fontSize: 20),
-            contentPadding:
-                const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-          )),
+        cursorColor: Colors.black54,
+        style: const TextStyle(
+          fontSize: 18,
+        ),
+        onFieldSubmitted: onFieldSubmitted,
+        onSaved: onSaved,
+        maxLines: null,
+        initialValue: initialValue,
+        keyboardType: textInputType,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return '$hint can\'t be null';
+          }
+          return null;
+        },
+        onChanged: onchanged,
+        onTapOutside: (event) {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        decoration: InputDecoration(
+          floatingLabelStyle: const TextStyle(color: Colors.blue),
+          label: Text(hint),
+          labelStyle: const TextStyle(fontSize: 20, color: Colors.black54),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(color: Colors.blue),
+          ),
+        ),
+      ),
     );
   }
 }
